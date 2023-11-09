f951c5aa1b864a7223ea0645a4177b7df7ca075b
Mwandotheboss <zephaniamwando@gmail.com>
2023-11-09 23:14:36 +0300

    Connecting React Front End to Node.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
+
+//Middleware
 app.use(express.json());
+app.use(cors());
 
 const db = require('./models')
 
