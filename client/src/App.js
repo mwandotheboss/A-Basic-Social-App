694b25327da5c1c4ff8c19047e1ce391782de83e
Mwandotheboss <zephaniamwando@gmail.com>
2023-11-22 23:55:46 +0300

    Login and register created

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Post from './pages/Post';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
 
 function App() {
   return (
@@ -11,11 +13,16 @@ function App() {
         <div className='navbar'>
           <Link to='/'> Home</Link>
           <Link to='/createpost'> Create Post</Link>
+          <Link to='/login'> Login</Link>
+          <Link to='/registration'> Create Account</Link>
         </div>
         <Routes>
           <Route path='/' exact element={< Home />} />
           <Route path='/createpost' element={< CreatePost />} />
           <Route path='/post/:id' element={< Post />} />
+          <Route path='/registration' element={< Registration />} />
+          <Route path='/login' element={< Login />} />
+
         </Routes>
       </Router>
     </div>
